cbfab17348e2e8d087de8770ac4ba80c0c990f62
baobao-ck <baobao@Baobei>
2025-08-12 22:18:43 +0700

    Add Remote backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tt-s3-pb-prod-ltt"
+    bucket         = "tt-s3-pb-prod-tfs"
     key            = "terraform.tfstate"  
     region         = "us-east-2"
+    use_lockfile   = true
     dynamodb_table = "tt-ddb-pb-prod-lt"
     encrypt        = true
   }
