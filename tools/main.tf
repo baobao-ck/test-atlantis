c1f314bd049348bc3f2c5dfc8a5cb3d6036a2fd7
baobao-ck <baobao@Baobei>
2025-08-13 17:31:55 +0700

    test dir

--- tools/main.tf
+++ tools/main.tf
@@ -2,14 +2,14 @@ resource "random_id" "bucket_suffix" {
   byte_length = 2
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "test-atlantis-bucket-${random_id.bucket_suffix.hex}"
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = "test-atlantis-bucket-${random_id.bucket_suffix.hex}"
 
-  tags = {
-    Name          = "Test-d"
-    ProvisionedBy = "Atlantis"
-  }
-}
+#   tags = {
+#     Name          = "Test-d"
+#     ProvisionedBy = "Atlantis"
+#   }
+# }
 
 resource "aws_dynamodb_table" "dynamodb_table" {
   name           = "my-table"
