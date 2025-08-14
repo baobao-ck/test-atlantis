226fb9849cb8b332527899fa079434b0ea8ffc6e
baobao-ck <baobao@Baobei>
2025-08-14 11:39:49 +0700

    test dir

--- tools/main.tf
+++ tools/main.tf
@@ -11,19 +11,19 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb_table" {
-  name           = "my-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "Id"
+# resource "aws_dynamodb_table" "dynamodb_table" {
+#   name           = "my-table"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 1
+#   write_capacity = 1
+#   hash_key       = "Id"
 
-  attribute {
-    name = "Id"
-    type = "S"
-  }
+#   attribute {
+#     name = "Id"
+#     type = "S"
+#   }
 
-  tags = {
-    Name = "MyTable"
-  }
-}
+#   tags = {
+#     Name = "MyTable"
+#   }
+# }
