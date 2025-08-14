01d8f2f75f9f9a54df293452a665f1e5ee14a728
baobao-ck <baobao@Baobei>
2025-08-14 14:12:21 +0700

    test dir

--- tools/main.tf
+++ tools/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "test-atlantis-bucket-${random_id.bucket_suffix.hex}"
 
   tags = {
-    Name          = "Test-test"
+    Name          = "Test-"
     ProvisionedBy = "Atlantis"
   }
 }
