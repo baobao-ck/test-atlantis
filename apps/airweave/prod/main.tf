887b9836afe1ce68be157b01798ca88fe304fad9
baobao-ck <baobao@Baobei>
2025-08-17 21:58:09 +0700

    test

---
+++ apps/airweave/prod/main.tf
@@ -0,0 +1,12 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "test-prod-airweave-bucket-${random_id.bucket_suffix.hex}"
+
+  tags = {
+    Name          = "TestS3"
+    ProvisionedBy = "Atlantis"
+  }
+}
