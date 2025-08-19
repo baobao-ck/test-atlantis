751ddb7750eca727cb6f920f3d684f8a2c9ef0de
baobao-ck <baobao@Baobei>
2025-08-19 09:15:11 +0700

    test-app-prod

--- apps/airweave/prod/main.tf
+++ apps/airweave/prod/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "test-prod-airweave-bucket-${random_id.bucket_suffix.hex}"
 
   tags = {
-    Name          = "Test-airweave-prod-sure"
+    Name          = "Test-airweave-prod-hichiLotie"
     ProvisionedBy = "Atlantis"
   }
 }
